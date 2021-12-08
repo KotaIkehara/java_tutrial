43dc60d14ad7ffd011ade31a031e1775fa4e1729
KotaIkehara <ikehara.aurora95@gmail.com>
2021-12-08 22:33:49 +0900

    finish chapter3

---
+++ chapter3/src/list3_1.java
@@ -0,0 +1,11 @@
+public class list3_1 {
+  public static void main(String[] args) {
+    boolean tenki = true;
+    if (tenki == true) {
+      System.out.println("選択をします");
+      System.out.println("散歩にいきます");
+    } else {
+      System.out.println("DVDを見ます");
+    }
+  }
+}

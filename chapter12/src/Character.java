2635aef587a593ddd80e54546827c463eb0c8b5e
KotaIkehara <ikehara.aurora95@gmail.com>
2021-12-14 22:21:22 +0900

    Finish cahpter13

---
+++ chapter12/src/Character.java
@@ -0,0 +1,15 @@
+public abstract class Character {
+  String name;
+  int hp;
+
+  // 逃げる
+  public void run() {
+    System.out.println(this.name + "は逃げ出した");
+  }
+
+  public abstract void attack(Matango m);
+
+  public String getName() {
+    return this.name;
+  }
+}

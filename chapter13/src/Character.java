ce0f4808392e32d231ee8d13f14c50215fe0f960
KotaIkehara <ikehara.aurora95@gmail.com>
2021-12-14 22:55:12 +0900

    list13-3

---
+++ chapter13/src/Character.java
@@ -0,0 +1,14 @@
+public abstract class Character {
+  String name;
+  int hp;
+
+  public abstract void attack(Matango m);
+
+  public void run() {
+    System.out.println(this.name + "は逃げ出した");
+  }
+
+  public String getName() {
+    return this.name;
+  }
+}
---
+++ chapter13/src/Fool.java
@@ -0,0 +1,17 @@
+public class Fool extends Character implements Human {
+  public void attack(Matango m) {
+    System.out.println(this.getName() + "戦わず遊んでいる．");
+  }
+
+  public void talk() {
+  };
+
+  public void watch() {
+  };
+
+  public void hear() {
+  };
+
+  public void run() {
+  };
+}

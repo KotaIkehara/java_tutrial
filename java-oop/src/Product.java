6cd98c406874b67ad729d25ff41c168b3c8e4d61
KotaIkehara <ikehara.aurora95@gmail.com>
2022-02-21 13:43:25 +0900

    Finish all

---
+++ java-oop/src/Product.java
@@ -0,0 +1,9 @@
+package sample;
+import java.time.LocalDate;
+public class Product {
+    private String number;
+    private String name;
+    private int price;
+    private LocalDate date;
+    private boolean stock;
+}
